fde0dca234a5920e1044ca3d0af7f1044a40fc9e
saianuragt <saianuragt@gmail.com>
2020-07-17 09:57:35 +0530

    FSD0620B105

--- dml-joins.sql
+++ dml-joins.sql
@@ -1,1 +1,352 @@
+--1. Write a query to return all the rows from user_new table and role tables where there is matching
+--   user_new.role_id value in both the usernew and role tables. Display role.name and user_new.name in ascending order.
+SELECT USER_NEW.NAME,ROLE.NAME
+FROM USER_NEW INNER JOIN ROLE
+ON USER_NEW.ID=ROLE.ID
+ORDER BY USER_NEW.NAME, ROLE.NAME ASC;
+--2. Write a query to return all rows from the user_new table and only those rows from the role table where the joined fields are equal.
+--   Display role.name and user_new in ascending order.
+SELECT * 
+FROM ROLE INNER JOIN USER_NEW
+ON USER_NEW.ID=ROLE.ID
+ORDER BY USER_NEW.NAME, ROLE.NAME ASC;
+--3. Write a query to return all names from the user_new table where there is matching user_new.role_id value in
+--   both the user_new and role tables.
+SELECT USER_NEW.NAME
+FROM ROLE INNER JOIN USER_NEW
+ON USER_NEW.ROLE_ID=ROLE.ID;
+--4. Write a query to return all  user_new.phonenumber, user_new.emailid, profile.batch, department.name, profile.designation,
+--   experience.company_name from user_new table where there is matching 
+--  - profile id in both user_new and profile table, 
+--  - profile.degree_id and degree.id from profile and degree table, 
+--  - degree.department_id and department.id in degree and department
+--  - profile.id and experience.profile_id in experience and profile table
+--  - where user_name.name = "ProGrad" and experience.currentValue=3
+SELECT USER_NEW.PHONENUMBER,PROFILE.BATCH,DEPARTMENT.NAME,PROFILE.DESIGNATION,EXPERIENCE.COMPANY_NAME
+FROM USER_NEW 
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE_ID
+INNER JOIN DEGREE ON PROFILE.DEGREE_ID=DEGREE.ID
+INNER JOIN DEPARTMENT ON DEGREE.DEPARTMENT_ID=DEPARTMENT.ID
+INNER JOIN EXPERIENCE ON PROFILE.ID=EXPERIENCE.PROFILE_ID
+WHERE USER_NEW.NAME='PROGRAD' AND EXPERIENCE.CURRENTVALUE=3;
+--5. Write a query to return all user_new.name, skill.name from user_new table where there are matching ids in the tables
+--   in the order with constraints given below
+--  - role
+--  - profile
+--  - profile_skills
+--  - where role.name="Alumini" 
+--  - Display by user_new.name and skill.name
+SELECT USER_NEW.NAME,SKILL.NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN SKILL ON PROFILE.ID=SKILL.ID
+WHERE ROLE.NAME='ALUMINI';
+--6. Write a query to return all user_new.name, experience.company_name from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - experience
+--  - where role.name = "Alumini"
+--  - Display in the order user_name.name followed by experience.company_name
+SELECT USER_NEW.NAME,EXPERIENCE.COMPANY_NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN EXPERIENCE ON USER_NEW.ID=EXPERIENCE.ID
+WHERE ROLE.NAME='ALUMINI';
+--7. Write a query to return all user_new.name, experience.company_name from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - experience
+--  - where role.name = "Alumini"
+--  - Display in the order user_name.name followed by experience.company_name
+SELECT USER_NEW.NAME,EXPERIENCE.COMPANY_NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN EXPERIENCE ON USER_NEW.ID=EXPERIENCE.ID
+WHERE ROLE.NAME='ALUMINI';
+--8. Write a query to return all user_new.name, experience.company_name from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - experience
+--  - where role.name='Alumni' and experience.currentValue=1
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME,EXPERIENCE.COMPANY_NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN EXPERIENCE ON USER_NEW.ID=EXPERIENCE.ID
+WHERE ROLE.NAME='ALUMINI' AND EXPERIENCE.CURRENTVALUE=1;
+--9. Write a query to return all user_new.name, experience.company_name from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - experience
+--  - where role.name='Alumni' and experience.currentValue=1 and profile.batch='2008' 
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME,EXPERIENCE.COMPANY_NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN EXPERIENCE ON USER_NEW.ID=EXPERIENCE.ID
+WHERE ROLE.NAME='ALUMINI' AND EXPERIENCE.CURRENTVALUE=1 AND PROFILE.BATCH='2008'
+ORDER BY USER_NEW.NAME;
+--10. Write a query to return all user_new.name, user_new.emailid, user_new.phonenumber, profile.address from user_new where
+--    there are matching ids in the tables 
+--  - role
+--  - profile
+--  - where role.name='Alumni' and user_new.profile_id is not null 
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, USER_NEW.EMAILID, USER_NEW.PHONENUMBER, PROFILE.ADDRESS
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+WHERE ROLE.NAME='ALUMINI' AND USER_NEW.PROFILE_ID IS NOT NULL
+ORDER BY USER_NEW.NAME;
+--11. Write a query to return all user_new.name, user_new.emailid, user_new.phonenumber, profile.address from
+--    user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - where role.name='Alumni' and profile.batch='2008'
+--  - Display in the order of user_new.name;     
+SELECT USER_NEW.NAME, USER_NEW.EMAILID, USER_NEW.PHONENUMBER, PROFILE.ADDRESS
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+WHERE ROLE.NAME='ALUMINI' AND PROFILE.BATCH='2008'
+ORDER BY USER_NEW.NAME;
+--12. Write a query to return all user_new.name, user_new.emailid, user_new.phonenumber, profile.address from 
+--    user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - degree
+--  - where role.name='Alumni' and degree.name='BSC_CT'
+--  - Display in the order of user_new.name;  
+SELECT USER_NEW.NAME, USER_NEW.EMAILID, USER_NEW.PHONENUMBER, PROFILE.ADDRESS
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+WHERE ROLE.NAME='ALUMINI' AND DEGREE.NAME='BSC_CT'
+ORDER BY USER_NEW.NAME;
+--13. Write a query to return alluser_new.name, user_new.emailid, user_new.phonenumber, profile.address from
+--    user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - degree
+--  - department
+--  - where role.name='Alumni' and department.name='CSE'
+--  - Display in the order of user_new.name;  
+SELECT USER_NEW.NAME, USER_NEW.EMAILID, USER_NEW.PHONENUMBER, PROFILE.ADDRESS
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+INNER JOIN DEPARTMENT ON USER_NEW.ID=DEPARTMENT.ID
+WHERE ROLE.NAME='ALUMINI' AND DEPARTMENT.NAME='CSE'
+ORDER BY USER_NEW.NAME;                  
+--14. Write a query to return all user_new.name, higher_degree.university_name from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - higher_degree
+--  - department
+--  - where role.name='Alumni' and higher_degree.degree_name is not null
+--  - Display in the order of user_new.name,higher_degree.university_name 
+SELECT USER_NEW.NAME, HIGHER_DEGREE.UNIVERSITY_NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN HIGHER_DEGREE ON USER_NEW.ID=HIGHER_DEGREE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+INNER JOIN DEPARTMENT ON USER_NEW.ID=DEPARTMENT.ID
+WHERE ROLE.NAME='ALUMINI' AND HIGHER_DEGREE.DEGREE_NAME IS NOT NULL
+ORDER BY USER_NEW.NAME,HIGHER_DEGREE.UNIVERSITY_NAME;
+--15. Write a query to return all user_new.name, higher_degree.university_name from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - higher_degree
+--  - department
+--  - where role.name='Alumni' and higher_degree.degree_name is not null
+--  - Display in the order of user_new.name,higher_degree.university_name 
+SELECT USER_NEW.NAME, HIGHER_DEGREE.UNIVERSITY_NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN HIGHER_DEGREE ON USER_NEW.ID=HIGHER_DEGREE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+INNER JOIN DEPARTMENT ON USER_NEW.ID=DEPARTMENT.ID
+WHERE ROLE.NAME='ALUMINI' AND HIGHER_DEGREE.DEGREE_NAME IS NOT NULL
+ORDER BY USER_NEW.NAME,HIGHER_DEGREE.UNIVERSITY_NAME;
+--16. Write a query to return all user_new.name, user_new.emailid, user_new.phonenumber, profile.address from user_new 
+--    where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - degree
+--  - department
+--  - higher_degree
+--  - where role.name='Alumni' and higher_degree.university_name='Texas University'
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, USER_NEW.EMAILID,USER_NEW.PHONENUMBER,PROFILE.ADDRESS
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+INNER JOIN DEPARTMENT ON USER_NEW.ID=DEPARTMENT.ID
+INNER JOIN HIGHER_DEGREE ON USER_NEW.ID=HIGHER_DEGREE.ID
+WHERE ROLE.NAME='ALUMINI' AND HIGHER_DEGREE.UNIVERSITY_NAME='TEXAS UNIVERSITY'
+ORDER BY USER_NEW.NAME;
+--17. Write a query to return all user_new.name, profile.batch from user_new where there are matching ids in the tables  
+--  - role
+--  - profile
+--  - where role.name='Alumni' and profile.gender='female'
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, PROFILE.BATCH
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+WHERE ROLE.NAME='ALUMINI' AND PROFILE.GENDER='FEMALE'
+ORDER BY USER_NEW.NAME;
+--18. Write a query to return all user_new.name, profile.batch, degree.name from user_new where there are matching ids in the tables  
+--  - role
+--  - profile
+--  - degree
+--  - where role.name='Alumni' and profile.gender='female'
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, PROFILE.BATCH,DEGREE.NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+WHERE ROLE.NAME='ALUMINI' AND PROFILE.GENDER='FEMALE'
+ORDER BY USER_NEW.NAME;
+--19. Write a query to return all user_new.name, profile.batch, degree.name, department.name from user_new where
+--    there are matching ids in the tables 
+--  - role
+--  - profile
+--  - degree
+--  - department
+--  - where role.name='Alumni' and profile.gender='female'
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, PROFILE.BATCH,DEGREE.NAME,DEPARTMENT.NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+INNER JOIN DEPARTMENT ON USER_NEW.ID=DEPARTMENT.ID
+WHERE ROLE.NAME='ALUMINI' AND PROFILE.GENDER='FEMALE'
+ORDER BY USER_NEW.NAME;
+--19. Write a query to return all degree.name, department.name from degree where there are matching ids in the tables 
+--  - department
+--  - Display in the order of degree.name;
+SELECT DEGREE.NAME, DEPARTMENT.NAME
+FROM DEGREE
+INNER JOIN DEPARTMENT ON DEGREE.DEPARTMENT_ID=DEPARTMENT.ID
+ORDER BY DEGREE.NAME;
+--20. Write a query to return all user_new.name, profile.designation from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - where role.name='Alumni' and profile.gender='male'
+--  - Display in the order of user_new.name;                                     
+SELECT USER_NEW.NAME,PROFILE.DESIGNATION
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+WHERE ROLE.NAME='ALUMNI' AND PROFILE.GENDER='MALE'
+ORDER BY USER_NEW.NAME;
+--21. Write a query to return all  user_new.name, profile.designation from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - experience
+--  - where role.name='Alumni' and experience.currentValue=1 and experience.company_name='TCS'
+--  - Display in the order of user_new.name; 
+SELECT USER_NEW.NAME,PROFILE.DESIGNATION
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN EXPERIENCE ON USER_NEW.ID=EXPERIENCE.ID
+WHERE ROLE.NAME='ALUMNI' AND EXPERIENCE.currentValue=1 AND EXPERIENCE.COMPANY_NAME='TCS'
+ORDER BY USER_NEW.NAME;
+--22. Write a query to return all  user_new.name, querytable.content from user_new where there are matching ids in the tables 
+--  - querytable
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, QUERYTABLE.CONTENT
+FROM USER_NEW
+INNER JOIN QUERYTABLE ON USER_NEW.ID=QUERYTABLE.ID
+ORDER BY USER_NEW.NAME;
+--23. Write a query to return all  user_new.name, querytable.content from user_new where there are matching ids in the tables 
+--  - querytable
+--  - where parent_id is not null
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, QUERYTABLE.CONTENT
+FROM USER_NEW
+INNER JOIN QUERYTABLE ON USER_NEW.ID=QUERYTABLE.ID
+WHERE QUERYTABLE.PARENT_ID IS NOT NULL
+ORDER BY USER_NEW.NAME;
+--24. Write a query to return all user_new.name, post.content from user_new where there are matching ids in the tables 
+--  - post
+--  - Display in the order of user_new.name;
+SELECT USER_NEW.NAME, POST.CONTENT
+FROM USER_NEW
+INNER JOIN POST ON USER_NEW.ID=POST.ID
+ORDER BY USER_NEW.NAME;          
+--25. Write a query to return all user_new.name, post.content from user_new where there are matching ids in the tables 
+--  - post
+--  - where post_type.name='Technology'
+--  - Display in the order of user_new.name,post.dateof;                                       
+SELECT USER_NEW.NAME, POST.CONTENT
+FROM USER_NEW
+INNER JOIN POST ON USER_NEW.ID=POST.ID
+INNER JOIN POST_TYPE ON POST.TYPE_ID=POST.ID
+WHERE POST_TYPE.NAME='TECHNOLOGY'
+ORDER BY USER_NEW.NAME, POST.DATEOF;
+--26. Write a query to return all user_new.name, post.content from user_new where there are matching ids in the tables 
+--  - post
+--  - where post.dateof='2013'
+--  - Display in the order of user_new.name, post.dateof, post.content;      
+SELECT USER_NEW.NAME, POST.CONTENT
+FROM USER_NEW
+INNER JOIN POST ON USER_NEW.ID=POST.ID
+WHERE POST.DATEOF='2013'
+ORDER BY USER_NEW.NAME, POST.DATEOF, POST.CONTENT;
+--27. Write a query to return all user_new.name, department.name from user_new where there are matching ids in the tables 
+--  - role
+--  - profile
+--  - degree
+--  - department
+--  - profile_skills
+--  - skill
+--  - where skill.name='Programming'
+--  - Display in the order of user_new.name;                         
+SELECT USER_NEW.NAME, DEPARTMENT.NAME
+FROM USER_NEW
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+INNER JOIN PROFILE ON USER_NEW.PROFILE_ID=PROFILE.ID
+INNER JOIN DEGREE ON USER_NEW.ID=DEGREE.ID
+INNER JOIN DEPARTMENT ON USER_NEW.ID=DEPARTMENT.ID
+INNER JOIN PROFILE_SKILLS ON USER_NEW.PROFILE_ID=PROFILE_SKILLS.PROFILE_ID
+INNER JOIN SKILL ON PROFILE_SKILLS.SKILL_ID=SKILL.ID
+WHERE SKILL.NAME='PROGRAMMING'
+ORDER BY USER_NEW.NAME;
+--28. Write a query to return all user_new.name, event.name from user_new where there are matching ids in the tables 
+--  - event
+--  - where role.name='Alumni'
+--  - Display in the order of user_new.name,event.dateof
+--  - Display event in descending order.
+SELECT USER_NEW.NAME, EVENT.NAME
+FROM USER_NEW
+INNER JOIN EVENT ON USER_NEW.ID=EVENT.ID
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+WHERE ROLE.NAME='ALUMNI'
+ORDER BY USER_NEW.NAME, EVENT.DATEOF, EVENT.NAME DESC;
+--29. Write a query to return all user_new.name, event.name from user_new where there are a matching ids in the tables 
+--  - event
+--  - role
+--  - where role.name='Alumni'
+--  - Display in the order of user_new.name,event.dateof
+SELECT USER_NEW.NAME, EVENT.NAME
+FROM USER_NEW
+INNER JOIN EVENT ON USER_NEW.ID=EVENT.ID
+INNER JOIN ROLE ON USER_NEW.ROLE_ID=ROLE.ID
+WHERE ROLE.NAME='ALUMNI'
+ORDER BY USER_NEW.NAME, EVENT.DATEOF;
 
--- insert-queries.sql
+++ insert-queries.sql
@@ -1,6 +1,10 @@
 insert into role(id,name)values(1,'designer');
 insert into role(id,name)values(2,'guide');
-
+insert into role(id,name)values(3,'ALUMINI');
+insert into role(id,name)values(4,'TEACHER');
+insert into role(id,name)values(5,'ALUMINI');
+insert into role(id,name)values(6,'STUDENT');
+insert into role(id,name)values(7,'ALUMINI');
 select * from role;
 
 insert into skill(id,description,name)values(1,'self learning','learn');
@@ -26,18 +30,48 @@ insert into higher_degree(id,degree_name,university_name,profile_id)values(1,'BE
 insert into higher_degree(id,degree_name,university_name,profile_id)values(2,'BE','STANFORD',2);
 
 
+insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(1,'TCS',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),1);
 insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(2,'CTS',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),2);
-
+insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(3,'Wipro',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),3);
+insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(4,'Accenture',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),4);
+insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(5,'Infosys',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),5);
+insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(6,'HCL',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),6);
+insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(7,'KPMG',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),7);
+select * from experience;
 
 insert into project(id,name,number_of_members,profile_id,short_description)values(1,'Alumini management',10,1,'for school and college');
 insert into project(id,name,number_of_members,profile_id,short_description)values(2,'Library management',11,2,'for school and college');
-
+insert into project(id,name,number_of_members,profile_id,short_description)values(3,'Department management',11,3,'for school and college');
+insert into project(id,name,number_of_members,profile_id,short_description)values(4,'Student management',11,4,'for school and college');
+insert into project(id,name,number_of_members,profile_id,short_description)values(5,'Hostel management',11,5,'for school and college');
+insert into project(id,name,number_of_members,profile_id,short_description)values(6,'Employee management',11,6,'for school and college');
+insert into project(id,name,number_of_members,profile_id,short_description)values(7,'Faculty management',11,7,'for school and college');
+select * from project;
 
 insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(1,'abcd@efgh.com','abcd','efgh',9884642005,1,1,'abcdefgh');
 insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(2,'ijkl@mnop.com','ijkl','mnop',9845621745,2,2,'ijklmnop');
+insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(3,'abc@xyz.com','abc','abc',9845621741,3,3,'abc');
+insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(4,'def@xyz.com','def','def',9845621742,4,4,'def');
+insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(5,'ghi@xyz.com','ghi','ghi',9845621743,5,5,'ghi');
+insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(6,'jkl@xyz.com','jkl','jkl',9845621744,6,6,'jkl');
+insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(7,'mno@xyz.com','mno','mno',9845621745,7,7,'mno');
+select * from user_new;
 
 insert into querytable(id,content,dateof,parent_id,user_id)values(1,'Alumini management',TO_DATE('2008/11/11','YYYY/MM/DD'),1,1);
 insert into querytable(id,content,dateof,parent_id,user_id)values(2,'Library management',To_DATE('2008/11/19','YYYY/MM/DD'),2,2);
+insert into querytable(id,content,dateof,parent_id,user_id)values(3,'Department management',To_DATE('2008/11/19','YYYY/MM/DD'),3,3);
+insert into querytable(id,content,dateof,parent_id,user_id)values(4,'Student management',To_DATE('2008/11/19','YYYY/MM/DD'),4,4);
+insert into querytable(id,content,dateof,parent_id,user_id)values(5,'Hostel management',To_DATE('2008/11/19','YYYY/MM/DD'),5,5);
+insert into querytable(id,content,dateof,parent_id,user_id)values(6,'Employee management',To_DATE('2008/11/19','YYYY/MM/DD'),6,6);
+insert into querytable(id,content,dateof,parent_id,user_id)values(7,'Faculty management',To_DATE('2008/11/19','YYYY/MM/DD'),7,7);
+select * from querytable;
 
- insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(1,TO_DATE('2008/11/11','YYYY/MM/DD'),'Alumni meet','OBA','ProGrad',1);
- insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(2,TO_DATE('2008/11/11','YYYY/MM/DD'),'Library opening','member','FACEPrep',2);
+
+insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(1,TO_DATE('2008/11/11','YYYY/MM/DD'),'Alumni meet','OBA','ProGrad',1);
+insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(2,TO_DATE('2008/11/11','YYYY/MM/DD'),'Library opening','member','FACEPrep',2);
+insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(3,TO_DATE('2008/11/11','YYYY/MM/DD'),'Building opening','member','FACEPrep',3);
+insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(4,TO_DATE('2008/11/11','YYYY/MM/DD'),'Hostel opening','member','FACEPrep',4);
+insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(5,TO_DATE('2008/11/11','YYYY/MM/DD'),'Mess opening','member','FACEPrep',5);
+insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(6,TO_DATE('2008/11/11','YYYY/MM/DD'),'Canteen opening','member','FACEPrep',6);
+insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(7,TO_DATE('2008/11/11','YYYY/MM/DD'),'Branch opening','member','FACEPrep',7);
+select * from event;
